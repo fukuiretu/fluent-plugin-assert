de7f2f19dc5b1ae5f06c5cffb23893fc89b6348d
FukuiRetu <s0232101@gmail.com>
2014-06-05 11:07:25 +0900

    write skeleton of plugin and its tests

--- lib/fluent/plugin/out_assert.rb
+++ lib/fluent/plugin/out_assert.rb
@@ -1,5 +1,37 @@
 module Fluent
   class AssertOutput < Fluent::BufferedOutput
-      # Your code goes here...
+    Fluent::Plugin.register_output('assert', self)
+
+    def initialize
+      super
+      # require 'hogepos'
+    end
+
+    def configure(conf)
+      super
+      # @path = conf['path']
+    end
+
+    def start
+      super
+      # init
+    end
+
+    def shutdown
+      super
+      # destroy
+    end
+
+    def format(tag, time, record)
+      [tag, time, record].to_msgpack
+    end
+
+    def write(chunk)
+      records = []
+      chunk.msgpack_each { |record|
+        # records << record
+      }
+      # write records
+    end
   end
 end
