21562f121a68ebd7bd361b85e6fbf41abeede78d
FukuiRetu <s0232101@gmail.com>
2014-06-05 13:08:30 +0900

    modify outline

--- lib/fluent/plugin/out_assert.rb
+++ lib/fluent/plugin/out_assert.rb
@@ -1,37 +1,86 @@
 module Fluent
-  class AssertOutput < Fluent::BufferedOutput
+  class AssertOutput < Fluent::Output
     Fluent::Plugin.register_output('assert', self)
+
+    MODE_LEN = 'len'
+    MODE_TYPE = 'type'
+    MODE_REG = 'reg'
+
+    config_param :add_prefix, :string, :default => nil
+    config_param :remove_prefix, :string, :default => nil
+
+
+    # Define `log` method for v0.10.42 or earlier
+    unless method_defined?(:log)
+      define_method("log") { $log }
+    end
 
     def initialize
       super
-      # require 'hogepos'
     end
 
     def configure(conf)
       super
-      # @path = conf['path']
+
+      if @remove_prefix
+        @removed_prefix_string = @remove_prefix + '.'
+      end
+
+      if @add_prefix
+        @added_prefix_string = @add_prefix + '.'
+      end
+
+      @cases = []
+      conf.elements.each do | element |
+        case element.name
+        when 'case'
+          @cases << element
+        else
+          raise Fluent::ConfigError, 'Unsupported Elements'
+        end
+      end
     end
 
-    def start
-      super
-      # init
-    end
+    def emit(tag, es, chain)
+      es.each { |time, record|
+        chain.next
 
-    def shutdown
-      super
-      # destroy
-    end
-
-    def format(tag, time, record)
-      [tag, time, record].to_msgpack
-    end
-
-    def write(chunk)
-      records = []
-      chunk.msgpack_each { |record|
-        # records << record
+        check record
       }
-      # write records
+    end
+
+    private
+
+    def check(record)
+      @cases.each do |element|
+        check_val = record[element['key']]
+
+        modes = element['mode'].split(',')
+        modes.each do |mode|
+          case mode
+          when MODE_LEN
+            check_len element, check_val
+          when MODE_TYPE
+            check_type element, check_val
+          when MODE_REG
+            check_reg element, check_val
+          else
+            # TODO エラー処理
+          end
+        end
+      end
+    end
+
+    def check_len(element, val)
+      p val
+    end
+
+    def check_type(element, val)
+      p val
+    end
+
+    def check_reg(element, val)
+      p val
     end
   end
 end
--- test/plugin/test_out_assert.rb
+++ test/plugin/test_out_assert.rb
@@ -6,51 +6,27 @@ class AssertOutputTest < Test::Unit::TestCase
   end
 
   CONFIG = %[
+add_prefix hoge
+<case>
+  mode len,type
+  key hoge
+  len 5 up
+  data_type integer
+  time_format yyyy-mm-dd
+</case>
   ]
-  # CONFIG = %[
-  #   path #{TMP_DIR}/out_file_test
-  #   compress gz
-  #   utc
-  # ]
 
   def create_driver(conf = CONFIG, tag='test')
-    Fluent::Test::BufferedOutputTestDriver.new(Fluent::AssertOutput, tag).configure(conf)
+    Fluent::Test::OutputTestDriver.new(Fluent::AssertOutput, tag).configure(conf)
   end
 
   def test_configure
-    #### set configurations
-    # d = create_driver %[
-    #   path test_path
-    #   compress gz
-    # ]
-    #### check configurations
-    # assert_equal 'test_path', d.instance.path
-    # assert_equal :gz, d.instance.compress
+    d = create_driver
   end
 
-  def test_format
+  def test_emit
     d = create_driver
-
-    # time = Time.parse("2011-01-02 13:14:15 UTC").to_i
-    # d.emit({"a"=>1}, time)
-    # d.emit({"a"=>2}, time)
-
-    # d.expect_format %[2011-01-02T13:14:15Z\ttest\t{"a":1}\n]
-    # d.expect_format %[2011-01-02T13:14:15Z\ttest\t{"a":2}\n]
-
-    # d.run
-  end
-
-  def test_write
-    d = create_driver
-
-    # time = Time.parse("2011-01-02 13:14:15 UTC").to_i
-    # d.emit({"a"=>1}, time)
-    # d.emit({"a"=>2}, time)
-
-    # ### FileOutput#write returns path
-    # path = d.run
-    # expect_path = "#{TMP_DIR}/out_file_test._0.log.gz"
-    # assert_equal expect_path, path
+    time = Time.parse("2012-01-02 13:14:15").to_i
+    d.emit({'hoge' => "abcdefg"}, time)
   end
 end
